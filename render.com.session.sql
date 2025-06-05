fd327e58d442e809241c322dc5c331efd1fc9fc1
emeksdot <emeksdot@yahoo.com>
2025-06-05 20:22:13 +0100

    tested the code and the home page displayed with tooltips

---
+++ render.com.session.sql
@@ -0,0 +1,16 @@
+INSERT INTO account (
+    account_id,
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password,
+    account_type
+  )
+VALUES (
+    account_id:integer,
+    'account_firstname:character varying',
+    'account_lastname:character varying',
+    'account_email:character varying',
+    'account_password:character varying',
+    'account_type:USER-DEFINED'
+  );
