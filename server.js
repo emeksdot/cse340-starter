8946a7379aa6ede88974204e14b30fcc1dee4bce
emeksdot <emeksdot@yahoo.com>
2025-06-07 23:20:32 +0100

    built the controllers

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const utilities = require("./utilities/")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -23,12 +25,36 @@ app.set("layout", "./layouts/layout")
  * Routes
  *************************/
 app.use(static)
-app.use(static)
 // Index or Home route
 // app.get("/", function (req, res) {
 //   res.render("index", {title: "Home"})
 // })
+
 app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
+app.use(async (req, res, next) => {
+  next({static: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+
+
+/* **********************************
+ *  Express Error Handler
+ *  Place after all other middleware
+ * ***************************************/
+
+
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
